f149777f74da4e026579278c5ae6595696ac6977
github username <github email address>
2019-11-06 09:01:49 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+
+  state={
+    pizzas: [],
+    activePizza: null
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pizzas')
+    .then(res => res.json())
+    .then(pizzas=>{
+      this.setState({
+        pizzas: [...pizzas]
+      })
+    })
+  }
+
+  changeActivePizza = id => {
+    this.setState({
+      activePizza: this.state.pizzas.find(pizza=>pizza.id === id)
+    })
+  }
+
+  mapTrick = newPizza => {
+    this.setState({
+      pizzas: this.state.pizzas.map(pizza=>{
+        if(pizza.id === newPizza.id) return newPizza
+        else return pizza
+      })
+    }) 
+  }
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm activePizza={this.state.activePizza} mapTrick={this.mapTrick}/>
+        <PizzaList pizzas={this.state.pizzas} changeActivePizza={this.changeActivePizza}/>
       </Fragment>
     );
   }
